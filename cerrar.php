593375a521601379770dcfafc4f4f592a666fe0c
JuliánMolinaJaramillo <judmolin@gmail.com>
2023-10-01 17:54:33 -0500

    Se actualiza proyecto

--- cerrar.php
+++ cerrar.php
@@ -3,5 +3,5 @@ session_start();
 //Destruimos la sesion, para destruir debemos iniciar
 session_destroy();
 //Funcion header nos permite enviar al usuario a una locación o direccion especifica
-header("location:login.php");
+echo '<script language="javascript">alert("Sesión Finalizada");window.location.href="login.php"</script>';
 ?>
